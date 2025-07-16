4c8c071cf2c17a8a8e6951e9c48007cb16310c56
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-07-16 11:21:25 +0200

    Updated Nav spacing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { IoIosPricetag } from "react-icons/io";
 const Navbar = () => {
   return (
     <nav className='bg-white shadow-md'>
-        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-0'>
             <div className='flex justify-between h-16'>
                 <div className='flex items-center'>
                     <div className='flex-shrink-0 flex items-center justify-center'>
