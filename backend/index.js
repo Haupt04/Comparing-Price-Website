595eefa22d310fc61cc5e152cf044d0395f752b6
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-07-23 13:15:00 +0200

    Backend Update to Packages

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,31 @@
 import express from 'express'
 import cors from 'cors'
 import compareRoute from './routes/compare.js'
+import emailRoute from './routes/emailRoute.js'
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config();
 
-
+const __dirname = path.resolve();
 const app = express();
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+
 
 app.use("/api/compare", compareRoute)
+app.use("/api/email", emailRoute)
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"))
+    })
+}
 
 
+// Error Handling
+app.use((err, req, res, next) => {
+    res.status(500).json({message: process.env.NODE_ENV === "production" ? "Internal Server Error": err.message})
+})
 
 app.listen(3001, () => console.log("Server is running on port 3001"))
