6542388273bc2dd6a1c98b1691d466d080792882
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-07-23 13:17:42 +0200

    Nav Bar and Heading Complete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
 import { IoIosPricetag } from "react-icons/io";
+
 
 const Navbar = () => {
   return (
-    <nav className='bg-white shadow-md'>
+    <nav className='bg-white shadow-md navbar'>
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-0'>
             <div className='flex justify-between h-16'>
                 <div className='flex items-center'>
                     <div className='flex-shrink-0 flex items-center justify-center'>
                         {/* Money Sign Logo */}
                         <IoIosPricetag className="h-8 w-8 text-green-600" />
-                        <span className='ml-2 text-xl font-bold text-gray-800'>PriceCompare - Amazon vs Takealot</span>
+                        <span  className='ml-2 text-xl sm:text-lg md:text-base font-bold text-gray-800'>PriceCompare - Amazon vs Takealot</span>
                     </div>
                 </div>
-                <div className='flex items-center'>
-                    {/* Need to change a to LINK */}
-                    <a href='#' className='px-3 py-2 text-sm font-medium text-gray-700 hover:text-green-600'>Home</a>
-                    <a href='#' className='px-3 py-2 text-sm font-medium text-gray-700 hover:text-green-600'>Categories</a>
-                    <a href='#' className='px-3 py-2 text-sm font-medium text-gray-700 hover:text-green-600'>Deals</a>
-                    <a href='#' className='px-3 py-2 text-sm font-medium text-gray-700 hover:text-green-600'>About</a>
+                <div className='hidden md:flex items-center'>
+                    <a href='#home' className='px-3 py-2 text-sm font-medium text-gray-700 hover:text-green-600'>Home</a>
+                    <a href='#popular-comparisons' className='px-3 py-2 text-sm font-medium text-gray-700 hover:text-green-600'>Featured Comparisons</a>
+                    <a href='#how-it-works' className='px-3 py-2 text-sm font-medium text-gray-700 hover:text-green-600'>How It Works</a>
                 </div>
             </div>
         </div>
