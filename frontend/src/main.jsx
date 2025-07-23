21eca6de9b965a97bcbe90103fd89efdc87b2c44
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-07-23 13:20:09 +0200

    Routers Completed amd Deployment Set Up

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { StrictMode } from 'react'
+import NotFound from './components/NotFound.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <Router>
+      <Routes>
+        <Route path="/" element={<App/>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
