f461a94db3361613d6b144bf939f6d69ed11be72
Haupt04 <116957227+Haupt04@users.noreply.github.com>
2025-07-23 13:32:44 +0200

    Debugging

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,15 @@ if (process.env.NODE_ENV === "production"){
     })
 }
 
+app._router.stack.forEach(middleware => {
+  if (middleware.route) { // routes registered directly on the app
+    console.log(middleware.route.path);
+  } else if (middleware.name === 'router') { // router middleware 
+    middleware.handle.stack.forEach(handler => {
+      if (handler.route) console.log(handler.route.path);
+    });
+  }
+});
 
 // Error Handling
 app.use((err, req, res, next) => {
